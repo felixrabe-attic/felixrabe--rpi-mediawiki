405d4c457ffd8664e8638ae9397df4d3f8d2cd81
Felix Rabe <felix@rabe.io>
2014-10-10 17:05:09 +0200

    start.sh: Make update.php --quick

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ echo "Args: $@"
 apache2ctl start
 /etc/init.d/mysql start
 mysql -u root -B <<<'create database if not exists my_wiki;'
-php /var/lib/mediawiki/maintenance/update.php
+php /var/lib/mediawiki/maintenance/update.php --quick
 echo
 echo 'Open http://<ip_of_raspberry_pi>/mediawiki/ in your browser.'
 while true ; do sleep 10 ; done
