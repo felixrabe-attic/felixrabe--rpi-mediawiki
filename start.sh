1a4a1896f8ed8575baee0063a4cd75f4ede4d643
Felix Rabe <felix@rabe.io>
2014-10-10 13:30:04 +0200

    start.sh: Abort on error, and create MySQL database

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+set -e
 echo "Args: $@"
 
 apache2ctl start
 /etc/init.d/mysql start
+mysql -u root -B <<'EOT'
+create database if not exists my_wiki;
+EOT
 echo
 echo 'Open http://<ip_of_raspberry_pi>/mediawiki/ in your browser.'
 while true ; do sleep 10 ; done
